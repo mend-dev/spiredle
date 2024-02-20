9f442cc44c26369dcdf3d69686e11f1e56c2fc50
mend-dev <mendbookdev@gmail.com>
2024-02-20 15:03:11 -0500

    Updates

---
+++ _app/immutable/chunks/paths.BwAGHu47.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1p98iae)==null?void 0:s.base)??"/spiredle";var e;const t=((e=globalThis.__sveltekit_1p98iae)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.BZZ-8EV3.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DWJvWipV.js";export{t as start};
