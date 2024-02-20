d6fcda3ebaa15e00ffab1b616ae508bba70cc87b
mend-dev <mendbookdev@gmail.com>
2024-02-19 21:20:15 -0500

    Removed base path config value

--- svelte.config.js
+++ svelte.config.js
@@ -18,10 +18,7 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/spiredle' : '',
-		}
+		})
 	}
 };
 export default config;
